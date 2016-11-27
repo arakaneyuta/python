a90a4bc876665f09624c06f19ef1e1337fd8cf3b
arakaneyuta <tomeyokohama@gmail.com>
2016-11-27 19:12:34 +0900

    Delete insertion_sort.py

--- insertion_sort.py
+++
@@ -1,9 +0,0 @@
-def insertion(array):
-	for j in range(0,len(array)):
-		for i in range(j,0,-1):
-			if array[i] < array[i-1]:
-				array[i],array[i-1] = array[i-1],array[i]
-	print array
-
-array = [42, 21, 10, 2, 30, 66, 100, 89, 43]
-insertion(array)
